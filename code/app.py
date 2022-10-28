f0c15570a409049724464f59047b478a1087df85
Domenico <55285326+mimmo96@users.noreply.github.com>
2022-10-28 15:40:30 +0200

    Add files via upload

---
+++ code/app.py
@@ -0,0 +1,22 @@
+import logging
+import os
+import json
+
+from flask import Flask, g
+
+logging.basicConfig(level=logging.DEBUG)
+secret_key = os.urandom(24).hex()  
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Welcome anon</a>'
+
+
+@app.route('/pippo')
+def pippo():
+    return 'ciao pippo'
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
