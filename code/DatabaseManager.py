37ac2cc8bc902c545d9a7343d0d5bc7d13d5c95e
Acciaro Gennaro Daniele <acciarogennaro@gmail.com>
2022-10-29 15:36:26 +0200

    Database

---
+++ code/DatabaseManager.py
@@ -0,0 +1,34 @@
+from mysql.connector import (connection)
+
+MySQL_host = "db4free.net"
+MySQL_user = "mariorui"
+MySQL_password = "mariorui"
+MySQL_db = "mariorui"
+
+def __open_connection():
+    try:
+        cnx = connection.MySQLConnection(user=MySQL_user, password=MySQL_password,
+                                         host=MySQL_host, database=MySQL_db)
+        cursor = cnx.cursor()
+    except:
+        print("Connection error")
+        return
+
+    return cnx, cursor
+
+def __close_connection(connection, cursor):
+    connection.commit()
+    cursor.close()
+    connection.close()
+
+def test_connection():
+    connection, cursor = __open_connection()
+    query = "select count(*) from tmp"
+    cursor.execute(query)
+
+    count = 0
+    for (c) in cursor:
+        count = c
+
+    __close_connection(connection, cursor)
+    return "Ci sono :"+str(count[0])+" record"
--- code/app.py
+++ code/app.py
@@ -1,6 +1,7 @@
 import logging
 import os
 import json
+import DatabaseManager
 
 from flask import Flask, g
 
@@ -13,10 +14,15 @@ app = Flask(__name__)
 def hello_world():
     return 'Welcome anon 2</a>'
 
-
 @app.route('/pippo')
 def pippo():
-    return 'ciao pippo'
+    return 'ciao pippo'\
+
+@app.route('/tmp_mysql')
+def pippone():
+    return 'ciao pippo'+DatabaseManager.test_connection()
 
 if __name__ == '__main__':
+    print( DatabaseManager.test_connection() )
+
     app.run(debug=True, host='0.0.0.0')
