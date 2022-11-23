8033ded253a8ded64f1f336bbe91a570b6b6feb0
mmc <martina.melerocavallo@outlook.com>
2022-11-23 18:26:29 +0100

    modified get_statistics and ml code

--- api/src/external_api/get_statistics.py
+++ api/src/external_api/get_statistics.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, jsonify, request
+from flask import Blueprint, jsonify, request, Response
 import pandas as pd
+
 statistics_from_ml_app = Blueprint('statistics_from_ml_app', __name__)
 
 from DatabaseManager import DatabaseManager
@@ -8,12 +9,12 @@ from utilities.utilities import check_params, convert_to_json
 
 db = DatabaseManager()
 
+
 @statistics_from_ml_app.route('/get_statistics', methods=['GET'])
 def get_test():
-
     params_list = ["asset", "start_date", "end_date", "part_program", "num_items"]
     valid_parameters, values = check_params(request, params_list)
-    
+
     if not valid_parameters:
         response = jsonify(['Precondition failed: parameters are not valid'])
         response.status_code = 412
@@ -25,24 +26,27 @@ def get_test():
     part_program = int(values['part_program'])
     num_items = int(values['num_items'])
 
-    #get data from db
-    query = "SELECT * FROM machine_data WHERE asset=%s AND part_program=%s AND" \
+    # get data from db
+    query = "SELECT * FROM machine_data WHERE asset LIKE %s AND part_program = %s AND" \
             " ts >= %s AND ts < %s ORDER BY ts;"
     records = db.query(query, (asset, part_program, start_date, end_date))
 
-    df = pd.DataFrame(records, columns = ['session','part_program','incremental_power_avg','incremental_items_avg','incremental_cycle_time_avg',
-                                            'power_var','cycle_var','asset','items','working_time','idle_time','power_avg','power_min','power_max',
-                                            'power_working','power_idle','cycle_time','alarm_1','alarm_2','alarm_3','alarm_4','part_program_y','predicted_alarm','ts'
+    if records != 'ERROR':
+        df = pd.DataFrame(records, columns=['session', 'part_program', 'incremental_power_avg', 'incremental_items_avg',
+                                            'incremental_cycle_time_avg', 'incremental_power',
+                                            'incremental_energy_cost', 'power_var', 'cycle_var', 'asset', 'items',
+                                            'working_time', 'idle_time', 'power_avg', 'power_min', 'power_max',
+                                            'power_working', 'power_idle', 'cycle_time', 'alarm_1', 'alarm_2',
+                                            'alarm_3', 'alarm_4', 'predicted_alarm', 'ts', 'energy_cost'
                                             ])
-    result = get_statistics(df, part_program, num_items)
+        result = get_statistics(df, part_program, num_items)
 
-    response = jsonify(result)
-    response.status_code = 200
-    return response
-
-    # If format is malformed or query doesn't end correctly
-    response = jsonify(['Bad request!'])
-    response.status_code = 400
-    return response
+        response = Response(result,  mimetype='application/json')
+        response.status_code = 200
+        return response
+    else:
+        # If format is malformed or query doesn't end correctly
+        response = jsonify(['Bad request!'])
+        response.status_code = 400
 
     return response
--- api/src/machine_learning/api_def.py
+++ api/src/machine_learning/api_def.py
@@ -17,66 +17,80 @@ Part program is needed because energy and time to build may differ from pp to pp
 '''
 
 
-def get_statistics(df ,part_program, num_items: int ):
-  #df = dataframe 
-  #pp = part program
-  #items = items that Production manager wants to produce
+def get_statistics(df, part_program, num_items: int):
+    # df = dataframe
+    # pp = part program
+    # items = items that Production manager wants to produce
 
-  pp = part_program
-  items = num_items
+    pp = part_program
+    items = num_items
 
-  dp = df[df['part_program'] == pp].copy()
-  pred_df = pd.DataFrame()
-  pred_df = pd.DataFrame(columns = ['items_min', 'pow_work_item','pow_idle_item', 'pow_avg_item', 'pow_max_item', 'pow_min_item'])
-  pred_df['items_min'] = dp['items']
-  pred_df['pow_work_item'] =  dp['power_working'] /  dp['items']
-  pred_df['pow_idle_item'] =  dp['power_idle'] /  dp['items']
-  
-  #calcolare questi valori dividendo per il numero di items forse non è corretto ?????
-  pred_df['pow_avg_item'] =  dp['power_avg'] /  dp['items']
-  pred_df['pow_max_item'] =  dp['power_max'] /  dp['items']
-  pred_df['pow_min_item'] =  dp['power_min'] /  dp['items']
+    dp = df[df['part_program'] == pp].copy()
+    pred_df = pd.DataFrame()
+    pred_df = pd.DataFrame(
+        columns=['items_min', 'pow_work_item', 'pow_idle_item', 'pow_avg_item', 'pow_max_item', 'pow_min_item'])
+    pred_df['items_min'] = dp['items']
+    pred_df['pow_work_item'] = dp['power_working'] / dp['items']
+    pred_df['pow_idle_item'] = dp['power_idle'] / dp['items']
 
-  pred_df['pow_work_item'] = pred_df['pow_work_item'].astype(float)
-  pred_df['pow_idle_item'] = pred_df['pow_idle_item'].astype(float)
-  pred_df['pow_avg_item'] = pred_df['pow_avg_item'].astype(float)
-  pred_df['pow_max_item'] = pred_df['pow_max_item'].astype(float)
-  pred_df['pow_min_item'] = pred_df['pow_min_item'].astype(float)
+    # calcolare questi valori dividendo per il numero di items forse non è corretto ?????
+    pred_df['pow_avg_item'] = dp['power_avg'] / dp['items']
+    pred_df['pow_max_item'] = dp['power_max'] / dp['items']
+    pred_df['pow_min_item'] = dp['power_min'] / dp['items']
 
-  stats = pd.DataFrame(pred_df.describe())
-  stats.drop(index=('count'), inplace = True)
+    pred_df['pow_work_item'] = pred_df['pow_work_item'].astype(float)
+    pred_df['pow_idle_item'] = pred_df['pow_idle_item'].astype(float)
+    pred_df['pow_avg_item'] = pred_df['pow_avg_item'].astype(float)
+    pred_df['pow_max_item'] = pred_df['pow_max_item'].astype(float)
+    pred_df['pow_min_item'] = pred_df['pow_min_item'].astype(float)
 
-  consumes = stats.copy()
-  consumes = pd.DataFrame(stats.loc['mean'])
-  consumes = consumes.loc[['pow_work_item', 'pow_idle_item', 'pow_avg_item']]
-  consumes['mean'] = consumes['mean']*items
-  consumes.loc['minutes'] = items / stats['items_min']
-  stats.reset_index(inplace=True)
-  stats = stats.rename(columns = {'index':'Stats'})
-  consumes.reset_index(inplace=True)
-  consumes = consumes.rename(columns = {'index':'Stats'})
+    stats = pd.DataFrame(pred_df.describe())
+    stats.drop(index=('count'), inplace=True)
 
-  output = []
-  for index, row in stats.iterrows():
-     output.append(str(row))
+    consumes = stats.copy()
+    consumes = pd.DataFrame(stats.loc['mean'])
+    consumes = consumes.loc[['pow_work_item', 'pow_idle_item', 'pow_avg_item']]
+    consumes['mean'] = consumes['mean'] * items
+    consumes.loc['minutes'] = items / stats['items_min']
+    stats.reset_index(inplace=True)
+    stats = stats.rename(columns={'index': 'Stats'})
+    consumes.reset_index(inplace=True)
+    consumes = consumes.rename(columns={'index': 'Stats'})
 
-  return output
-  
-  return [stats.to_json(orient="records"), consumes.to_json(orient="records")]
+    output = pd.DataFrame(stats[['Stats', 'pow_work_item', 'pow_avg_item']]).set_index('Stats')
+    print(output)
 
-  return [ 
-            {"Stats":"mean","items_min":2.9983193277,"pow_work_item":809.4861690011,"pow_idle_item":0.0,"pow_avg_item":808.8481652661,"pow_max_item":3774.4410784314,"pow_min_item":94.4581938375},
-            {"Stats":"std","items_min":0.0579771036,"pow_work_item":59.9560920919,"pow_idle_item":0.0,"pow_avg_item":42.8009710656,"pow_max_item":354.2711525155,"pow_min_item":9.2896743181},
-            {"Stats":"min","items_min":1.0,"pow_work_item":691.268,"pow_idle_item":0.0,"pow_avg_item":691.27,"pow_max_item":2745.67,"pow_min_item":81.67},
-            {"Stats":"25%","items_min":3.0,"pow_work_item":787.3575,"pow_idle_item":0.0,"pow_avg_item":787.3566666667,"pow_max_item":3570.1333333333,"pow_min_item":89.448},
-            {"Stats":"50%","items_min":3.0,"pow_work_item":809.812,"pow_idle_item":0.0,"pow_avg_item":809.8116666667,"pow_max_item":3706.2666666667,"pow_min_item":93.3373333333},
-            {"Stats":"75%","items_min":3.0,"pow_work_item":827.59,"pow_idle_item":0.0,"pow_avg_item":827.59,"pow_max_item":3993.0916666667,"pow_min_item":97.2263333333},
-            {"Stats":"max","items_min":3.0,"pow_work_item":2576.178,"pow_idle_item":0.0,"pow_avg_item":1822.18,"pow_max_item":10920.5,"pow_min_item":326.68},
-            {"Stats":"pow_work_item","mean":80948.6169001121},
-            {"Stats":"pow_idle_item","mean":0.0},
-            {"Stats":"pow_avg_item","mean":80884.8165266106},
-            {"Stats":"minutes","mean":33.3520179372}     
+    return output.to_json()
+
+    output = []
+    for index, row in stats.iterrows():
+        output.append(str(row))
+
+    # return output
+
+    return [stats.to_json(orient="records"), consumes.to_json(orient="records")]
+
+    return [
+        {"Stats": "mean", "items_min": 2.9983193277, "pow_work_item": 809.4861690011, "pow_idle_item": 0.0,
+         "pow_avg_item": 808.8481652661, "pow_max_item": 3774.4410784314, "pow_min_item": 94.4581938375},
+        {"Stats": "std", "items_min": 0.0579771036, "pow_work_item": 59.9560920919, "pow_idle_item": 0.0,
+         "pow_avg_item": 42.8009710656, "pow_max_item": 354.2711525155, "pow_min_item": 9.2896743181},
+        {"Stats": "min", "items_min": 1.0, "pow_work_item": 691.268, "pow_idle_item": 0.0, "pow_avg_item": 691.27,
+         "pow_max_item": 2745.67, "pow_min_item": 81.67},
+        {"Stats": "25%", "items_min": 3.0, "pow_work_item": 787.3575, "pow_idle_item": 0.0,
+         "pow_avg_item": 787.3566666667, "pow_max_item": 3570.1333333333, "pow_min_item": 89.448},
+        {"Stats": "50%", "items_min": 3.0, "pow_work_item": 809.812, "pow_idle_item": 0.0,
+         "pow_avg_item": 809.8116666667, "pow_max_item": 3706.2666666667, "pow_min_item": 93.3373333333},
+        {"Stats": "75%", "items_min": 3.0, "pow_work_item": 827.59, "pow_idle_item": 0.0, "pow_avg_item": 827.59,
+         "pow_max_item": 3993.0916666667, "pow_min_item": 97.2263333333},
+        {"Stats": "max", "items_min": 3.0, "pow_work_item": 2576.178, "pow_idle_item": 0.0, "pow_avg_item": 1822.18,
+         "pow_max_item": 10920.5, "pow_min_item": 326.68},
+        {"Stats": "pow_work_item", "mean": 80948.6169001121},
+        {"Stats": "pow_idle_item", "mean": 0.0},
+        {"Stats": "pow_avg_item", "mean": 80884.8165266106},
+        {"Stats": "minutes", "mean": 33.3520179372}
     ]
+
 
 '''
 Serve some new data to the ML module in form of a pandas DataFrame.
@@ -89,20 +103,24 @@ for the production manager
 :return: either an empty list (means everithing is fine) or a list 
 of messages from the ML module, signaling anomalies found in the data.
 '''
+
+
 def give_new_data_to_ml(new_data: pd.DataFrame):
-    
     # here we will process the new data
 
     return fetch_messages_from_ml_module()
+
 
 '''
 Very primitive method, just to Give the idea. Call to get messages from the ML team.
 Messages are are just text with a timestamp (ts) to be sure you dont'display the same one
 more than once
 '''
+
+
 def fetch_messages_from_ml_module():
-  return [
-    {
-      'message':'https://www.youtube.com/watch?v=zB_q1I0leoI','ts': dt.now()
-    },
-  ]
+    return [
+        {
+            'message': 'https://www.youtube.com/watch?v=zB_q1I0leoI', 'ts': dt.now()
+        },
+    ]
