d9bcdb57a29410126da0ad940a1bec37d4db3a33
Domenico Profumo <domenico.profumo@gmail.com>
2022-11-23 17:21:26 +0100

    update ml get_statistics (da rivedere)

--- api/src/machine_learning/api_def.py
+++ api/src/machine_learning/api_def.py
@@ -17,8 +17,53 @@ Part program is needed because energy and time to build may differ from pp to pp
 '''
 
 
-def get_statistics(start: dt, end: dt, part_program: str, asset: str, num_items: int ):
+def get_statistics(df ,part_program, num_items: int ):
+  #df = dataframe 
+  #pp = part program
+  #items = items that Production manager wants to produce
+
+  pp = part_program
+  items = num_items
+
+  dp = df[df['part_program'] == pp].copy()
+  pred_df = pd.DataFrame()
+  pred_df = pd.DataFrame(columns = ['items_min', 'pow_work_item','pow_idle_item', 'pow_avg_item', 'pow_max_item', 'pow_min_item'])
+  pred_df['items_min'] = dp['items']
+  pred_df['pow_work_item'] =  dp['power_working'] /  dp['items']
+  pred_df['pow_idle_item'] =  dp['power_idle'] /  dp['items']
   
+  #calcolare questi valori dividendo per il numero di items forse non è corretto ?????
+  pred_df['pow_avg_item'] =  dp['power_avg'] /  dp['items']
+  pred_df['pow_max_item'] =  dp['power_max'] /  dp['items']
+  pred_df['pow_min_item'] =  dp['power_min'] /  dp['items']
+
+  pred_df['pow_work_item'] = pred_df['pow_work_item'].astype(float)
+  pred_df['pow_idle_item'] = pred_df['pow_idle_item'].astype(float)
+  pred_df['pow_avg_item'] = pred_df['pow_avg_item'].astype(float)
+  pred_df['pow_max_item'] = pred_df['pow_max_item'].astype(float)
+  pred_df['pow_min_item'] = pred_df['pow_min_item'].astype(float)
+
+  stats = pd.DataFrame(pred_df.describe())
+  stats.drop(index=('count'), inplace = True)
+
+  consumes = stats.copy()
+  consumes = pd.DataFrame(stats.loc['mean'])
+  consumes = consumes.loc[['pow_work_item', 'pow_idle_item', 'pow_avg_item']]
+  consumes['mean'] = consumes['mean']*items
+  consumes.loc['minutes'] = items / stats['items_min']
+  stats.reset_index(inplace=True)
+  stats = stats.rename(columns = {'index':'Stats'})
+  consumes.reset_index(inplace=True)
+  consumes = consumes.rename(columns = {'index':'Stats'})
+
+  output = []
+  for index, row in stats.iterrows():
+     output.append(str(row))
+
+  return output
+  
+  return [stats.to_json(orient="records"), consumes.to_json(orient="records")]
+
   return [ 
             {"Stats":"mean","items_min":2.9983193277,"pow_work_item":809.4861690011,"pow_idle_item":0.0,"pow_avg_item":808.8481652661,"pow_max_item":3774.4410784314,"pow_min_item":94.4581938375},
             {"Stats":"std","items_min":0.0579771036,"pow_work_item":59.9560920919,"pow_idle_item":0.0,"pow_avg_item":42.8009710656,"pow_max_item":354.2711525155,"pow_min_item":9.2896743181},
