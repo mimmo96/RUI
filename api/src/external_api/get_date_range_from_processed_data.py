6adf4655fe2ac21dc0943b0eb1fe59fdbcd7037c
Domenico Profumo <domenico.profumo@gmail.com>
2023-01-11 14:32:30 +0100

    fixed get data range from processed data

--- api/src/external_api/get_date_range_from_processed_data.py
+++ api/src/external_api/get_date_range_from_processed_data.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, jsonify, request
 
-from utilities.utilities import checkvalue, convert_to_json, check_params
+from utilities.utilities import check_params
 from DatabaseManager import DatabaseManager
 db = DatabaseManager()
 
@@ -23,10 +23,49 @@ def get_date_range_from_processed_data():
     records = db.query(query, (start_date, end_date))
 
     if str(records).upper() != "ERROR":
-        response = jsonify(convert_to_json(records))
+        response = jsonify(convert_to_json1(records))
         response.status_code = 200
     else:
         response = jsonify(['Bad request!'])
         response.status_code = 400
 
     return response
+
+def convert_to_json1(values):
+      
+    data = [{"session": session,
+             "part_program": part_program,
+             "incremental_power_avg": incremental_power_avg,
+             "incremental_items_avg": incremental_items_avg,
+             "incremental_cycle_time_avg": incremental_cycle_time_avg,
+             "incremental_power": incremental_power,
+             "incremental_energy_cost": incremental_energy_cost,
+             "power_var": power_var,
+             "cycle_var": cycle_var,
+             "asset": asset,
+             "items": items,
+             "working_time": working_time,
+             "idle_time": idle_time,
+             "power_avg": power_avg,
+             "power_min": power_min, "power_max": power_max, "power_working": power_working,
+             "power_idle": power_idle, "cycle_time": cycle_time,
+             "alarm_1": alarm_1,'alarm_2': alarm_2 ,'alarm_3': alarm_3 ,'alarm_4': alarm_4 ,
+             'predicted_alarm': predicted_alarm ,
+             'ts': ts,
+             'energy_cost': energy_cost,
+             'machine_state' : machine_state
+             }
+            for session, part_program,
+                incremental_power_avg,
+                incremental_items_avg,
+                incremental_cycle_time_avg,
+                incremental_power, incremental_energy_cost,
+                power_var,
+                cycle_var, asset,
+                items, working_time,
+                idle_time, power_avg,
+                power_min, power_max,
+                power_working, power_idle, cycle_time,
+                alarm_1, alarm_2, alarm_3, alarm_4, predicted_alarm, ts, energy_cost,machine_state in values]
+
+    return data
